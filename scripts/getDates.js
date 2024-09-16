7910f06d6b350e83bb1f622f581547df116fef30
NicolasV99 <legolasnicolas99@gmail.com>
2024-09-15 19:53:25 -0500

    create index, add images, and implement the homepage

---
+++ scripts/getDates.js
@@ -0,0 +1,9 @@
+// Update the current year
+const currentYearElement = document.getElementById('currentYear');
+const currentYear = new Date().getFullYear();
+currentYearElement.textContent = currentYear;
+
+// Update the last modified date
+const lastModifiedElement = document.getElementById('lastModified');
+const lastModifiedDate = new Date(document.lastModified);
+lastModifiedElement.textContent = `Last Modification: ${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
