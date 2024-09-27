f6969842a43026fdaa7e3f72782afffe324e023d
NicolasV99 <legolasnicolas99@gmail.com>
2024-09-26 20:11:30 -0500

    fix hamburguer menu and add normalize css

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -9,8 +9,20 @@ const lastModifiedDate = new Date(document.lastModified);
 lastModifiedElement.textContent = `Last Modification: ${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
 
 //Action for open or close menu when click to the menu button
-document.getElementById('menu-button').addEventListener('click', function() {
-    document.getElementById('menu-items').classList.toggle('open');
+const menuButton = document.getElementById('menu-toggle');
+const menuItems = document.querySelector('.menu');
+
+menuButton.addEventListener('click', function() {
+    // Alternar clases 'open' en el botón y el menú
+    menuItems.classList.toggle('open');
+    menuButton.classList.toggle('open');
+
+    // Cambiar el icono del botón de hamburguesa
+    if (menuButton.classList.contains('open')) {
+        menuButton.textContent = '✖'; // Mostrar "X"
+    } else {
+        menuButton.textContent = '☰'; // Volver al icono de hamburguesa
+    }
 });
 
 document.getElementById('dark-mode-toggle').addEventListener('click', function() {
