88b13b54f52c550ef84a039804f4c2a35e4d7bee
NicolasV99 <legolasnicolas99@gmail.com>
2024-09-26 20:00:19 -0500

    add a favicon, hamburguer button and dark mode to the site

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,3 +7,12 @@ currentYearElement.textContent = currentYear;
 const lastModifiedElement = document.getElementById('lastModified');
 const lastModifiedDate = new Date(document.lastModified);
 lastModifiedElement.textContent = `Last Modification: ${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
+
+//Action for open or close menu when click to the menu button
+document.getElementById('menu-button').addEventListener('click', function() {
+    document.getElementById('menu-items').classList.toggle('open');
+});
+
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
