2d3247e8618fe0a3cd2d2c52a835b3cd9d0b2070
NicolasV99 <legolasnicolas99@gmail.com>
2024-09-28 17:33:45 -0500

    header part section done

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -7,3 +7,13 @@ currentYearElement.textContent = currentYear;
 const lastModifiedElement = document.getElementById('lastModified');
 const lastModifiedDate = new Date(document.lastModified);
 lastModifiedElement.textContent = `Last Modification: ${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
+
+
+
+// Toggle burger menu
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+});
