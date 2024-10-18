c247118b9e6b2f5b481b035859ac7ab86a6945b7
NicolasV99 <legolasnicolas99@gmail.com>
2024-10-17 19:52:24 -0500

    add week03 - week 04 activities

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -3,14 +3,28 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 const cards = document.querySelector('#cards');
 
 async function getProphetData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    //console.table(data.prophets);
-    displayProphets(data.prophets);
+    try {
+        // Realiza la solicitud fetch para obtener los datos
+        const response = await fetch(url);
+    
+        // Convierte la respuesta a formato JSON
+        const data = await response.json();
+    
+        // Muestra los datos en formato tabla en la consola (opcional para verificar)
+        console.table(data.prophets);
+    
+        // Llama a la función displayProphets pasando el array de profetas
+        displayProphets(data.prophets);
+    
+      } catch (error) {
+        console.error('Error fetching the data:', error);
+      }
 }
 
 const displayProphets = (prophets) => {
-    
+    prophets.forEach(element => {
+        
+    });
 }
 
 getProphetData();
